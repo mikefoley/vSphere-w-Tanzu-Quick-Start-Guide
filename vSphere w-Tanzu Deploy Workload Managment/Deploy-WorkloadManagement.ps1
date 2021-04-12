706d670f98ad5785108500c268f2cbca68106718
Mike Foley <mike@yelof.com>
2021-04-12 15:26:57 -0700

    Updated connect-viserver logic

--- vSphere w-Tanzu Deploy Workload Managment/Deploy-WorkloadManagement.ps1
+++ vSphere w-Tanzu Deploy Workload Managment/Deploy-WorkloadManagement.ps1
@@ -28,7 +28,14 @@ Param(
     [string]$vc_password = "VMware1!"
     )
 
-Connect-VIServer -User $vc_user -Password $vc_password -Server $vc
+
+    if ($global:DefaultVIServers) {
+        Disconnect-VIServer -Server $global:DefaultVIServers -Force -confirm:$false 
+    }
+    else {
+        Connect-VIServer -User $vc_user -Password $vc_password -Server $vc 
+    }
+    
 $Cluster = Get-Cluster  -Name "cluster"
 $datacenter = Get-Datacenter "datacenter"
 $datastore = Get-Datastore -Name  "vsanDatastore"
